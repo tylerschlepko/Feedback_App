e9ded2bc8301711c3183b115564a8fdaca083510
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-10 09:21:31 -0600

    initial push

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,20 @@
+import { FaTimes } from 'react-icons/fa'
+import Card from './Shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedbackItem({item, handleDelete}) {
+
+    return (
+        <Card >
+            <div className="num-display">{item.rating}</div>
+            <button className="close" onClick={() => handleDelete(item.id)}><FaTimes color='purple'/></button>
+            <div className="text-display">{item.text}</div>
+        </Card>
+    )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+
+export default FeedbackItem
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function FeedbackStats({feedback}) {
+    //calculate ratings average
+    let average = feedback.reduce((acc, curr) => {
+        return acc + curr.rating
+    }, 0) / feedback.length
+
+    average = average.toFixed(1).replace(/[.,]0$/, '')
+
+    console.log(average);
+
+  return (
+    <div className='feedback-stats'>
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
+}
+
+export default FeedbackStats
